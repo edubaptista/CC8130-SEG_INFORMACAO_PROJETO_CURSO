f48db2cc757e05474bbace1b1466f869d2cdaa54
edubaptista <43965828+edubaptista@users.noreply.github.com>
2021-05-30 22:23:18 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+ArquivoBaseEntrada = r'C:\Users\Eduardo Baptista\Desktop\Ciência da Computação - FEI\8º Ciclo\CC8130 - SEGURANÇA NA INFORMAÇÃO\Projeto de curso\Código\base.txt'
+ArquivoBaseSeguraSaida = r'C:\Users\Eduardo Baptista\Desktop\Ciência da Computação - FEI\8º Ciclo\CC8130 - SEGURANÇA NA INFORMAÇÃO\Projeto de curso\Código\baseConvertida.txt'
+Salt = "FEI"
